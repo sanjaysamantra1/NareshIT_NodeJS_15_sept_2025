d988463cd749bba0d60f07220a4a9aa0c9989f8d
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-10-18 08:57:18 +0530

    day 26

---
+++ day_25_oct_17/joi_demo.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const Joi = require('joi');
+const server = express();
+let employees = [
+    { "eId": 101, "name": "sanjay", "sal": 5000, "gender": "male" },
+    { "eId": 104, "name": "geeta", "sal": 8000, "gender": "female" },
+    { "eId": 103, "name": "sameer", "sal": 7000, "gender": "male" },
+    { "eId": 102, "name": "sita", "sal": 9000, "gender": "female" },
+    { "eId": 105, "name": "deepak", "sal": 8000, "gender": "male" }
+];
+
+const createEmpSchema = Joi.object({
+    name: Joi.string()
+        .alphanum()
+        .min(5)
+        .max(30)
+        .required(),
+    sal: Joi.number()
+        .min(0)
+        .required()
+});
+
+server.get("/employees", (req, res) => {
+    res.json(employees);
+});
+server.get("/employeeInfo/:id?", (req, res) => {
+    const empId = parseInt(req.params.id);
+    const employee = employees.find(emp => emp.eId === empId);
+    if (employee) {
+        res.json(employee);
+    } else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+});
+server.post("/employees", express.json(), (req, res) => {
+    let { error, value } = createEmpSchema.validate(req.body);
+    if (error) {
+        res.status(400).json({ erros:error.details })
+    } else {
+        const newEmployee = value;
+        employees.push(newEmployee);
+        res.status(201).json(newEmployee);
+    }
+});
+server.patch("/employees/:id", express.json(), (req, res) => {
+    const empId = parseInt(req.params.id);
+    const employee = employees.find(emp => emp.eId === empId);
+    if (employee) {
+        Object.assign(employee, req.body);
+        res.json(employee);
+    } else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+});
+server.delete("/employees/:id", (req, res) => {
+    const empId = parseInt(req.params.id);
+    const index = employees.findIndex(emp => emp.eId === empId);
+    if (index !== -1) {
+        employees.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+});
+server.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+/* 
+    1. Get all employees
+    2. Get employee by id
+    3. Add new employee (Create)
+    4. Update employee by id
+    5. Delete employee by id
+*/
---
+++ day_26_oct_18/node_typescript_demo/src/app.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+
+const server = express();
+
+let employees = [
+    { "eId": 101, "name": "sanjay", "sal": 5000, "gender": "male" },
+    { "eId": 104, "name": "geeta", "sal": 8000, "gender": "female" },
+    { "eId": 103, "name": "sameer", "sal": 7000, "gender": "male" },
+    { "eId": 102, "name": "sita", "sal": 9000, "gender": "female" },
+    { "eId": 105, "name": "deepak", "sal": 8000, "gender": "male" }
+];
+server.get("/employees", (req: any, res: any) => {
+    res.json(employees);
+});
+server.get("/employees/:id", (req: any, res: any) => {
+    const empId = parseInt(req.params.id);
+    const employee = employees.find(emp => emp.eId === empId);
+    if (employee) {
+        res.json(employee);
+    } else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+});
+/* server.post("/employees", express.json(), (req, res) => {
+    const newEmployee = req.body;
+    employees.push(newEmployee);
+    res.status(201).json(newEmployee);
+});
+server.patch("/employees/:id", express.json(), (req, res) => {
+    const empId = parseInt(req.params.id);
+    const employee = employees.find(emp => emp.eId === empId);
+    if (employee) {
+        Object.assign(employee, req.body);
+        res.json(employee);
+    } else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+});
+server.delete("/employees/:id", (req, res) => {
+    const empId = parseInt(req.params.id);
+    const index = employees.findIndex(emp => emp.eId === empId);
+    if (index !== -1) {
+        employees.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).json({ message: "Employee not found" });
+    }
+}); */
+server.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+/* 
+    1. Get all employees
+    2. Get employee by id
+    3. Add new employee (Create)
+    4. Update employee by id
+    5. Delete employee by id
+*/
