3b40d209f5e8d2990de914d23c26756b8ab8e877
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-10-30 11:07:05 +0530

    day 35

--- day_35_oct_30/products/products_script.js
+++ day_35_oct_30/products/products_script.js
@@ -1,9 +1,17 @@
-// print the count of each category
+// Total Number of Products Per Category
+db.products.aggregate([
+    { $group: { _id: '$category', totalProducts: { $sum: 1 } } }
+])
 
 // Calculate the Average Price per Category
+db.products.aggregate([
+    { $group: { _id: '$category', avgPrice: { $avg:'$price' } } }
+])
 
-// print Sum of Product Stock in Each Category
-
+// print Sum of Product Stock in Each Category (totalStock per category)
+db.products.aggregate([
+    { $group: { _id: '$category', totalStock: { $sum: '$quantity' } } }
+])
 
 // Print name,price for the products of category-electronics
 
@@ -12,7 +20,7 @@
 // product_name is alias for name , product_price is alias for price
 
 
-// Print name,price,discountedPrice(10%) for the products 
+// Print name,price,discountedPrice(10%) for the products
 
 
 // Calculate Discounted Price and amountSaved
