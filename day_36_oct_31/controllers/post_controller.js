ba61a64cbe366467dee4d165e13562ba5fad62ac
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-11-03 08:12:54 +0530

    populate

--- day_36_oct_31/controllers/post_controller.js
+++ day_36_oct_31/controllers/post_controller.js
@@ -2,8 +2,7 @@ const postModel = require('../models/post_model')
 
 exports.getAllPosts = async (req, res) => {
     try {
-        const posts = await postModel.find().populate('author');
-        console.log(posts)
+        const posts = await postModel.find().populate('author','email password');
         res.json(posts);
     } catch (err) {
         console.error(err);
