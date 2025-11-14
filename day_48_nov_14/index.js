488e6de945177e1cebf155e2f7e6cb9bf76db363
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-11-14 08:26:34 +0530

    day 48

---
+++ day_48_nov_14/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { employeeRouter } from "./src/routes/employee_routes.js";
+
+export const app = express();
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+async function connectToDB() {
+    await mongoose.connect('mongodb://127.0.0.1/nareshit_sept_2025');
+    console.log('Database connection established')
+}
+app.use('/employees', employeeRouter)
+app.listen(3000, async () => {
+    await connectToDB();
+    console.log('app running at 3000')
+});
+
---
+++ day_48_nov_14/test/employee_controller.test.js
@@ -0,0 +1,11 @@
+import * as chai from "chai";
+import { default as chaiHttp, request } from "chai-http";
+import sinon from "sinon";
+chai.use(chaiHttp)
+
+describe('Test Suite for Employee CRUD', () => {
+    it('Should Verify getAllEmployees', () => {
+        console.log('It for getAllEmployees')
+    })
+})
+
